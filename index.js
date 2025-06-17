6e4a40258b7d0a6ef15af2da1d5043f33be48b3d
zooma123 <93126188+zooma123@users.noreply.github.com>
2025-06-17 21:03:49 +0300

    Initilize The Project In Back End
    Install The Packages And Install Server And Connect To Db Mongoo

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const  mongoose = require("mongoose");
+const url = require("url");
+const app = express();
+const  cors = require ("cors") ;
+const  dotenv = require ('dotenv') ;    
+const cookieParser = require('cookie-parser')
+app.use(cors());
+dotenv.config();
+const { createServer } = require('node:http');
+ const server = createServer(app);
+
+app.use(express.json())
+app.use(cookieParser());
+app.use(express.urlencoded({extended :true}))
+
+
+
+const port = process.env.PORT;
+server.listen(port, async () =>{
+console.log(`app is  Running on ${port}`);
+
+});
+
+
+
+
+mongoose.connect(process.env.MONGO_URI,{
+}).then(con=>{
+console.log("Connected To Data Base")
+  
+})
